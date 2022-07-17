f68f33ff4ecf2d1aa6b17dd604ee9fdc2fef0bd6
EryX666 <omerbar7193@gmail.com>
2022-07-17 19:41:35 +0300

    . .

--- src/App.js
+++ src/App.js
@@ -7,20 +7,22 @@ import PhotoSection from "./Components/PhotoSection";
 import ContactSection from "./Components/ContactSection";
 import Footer from "./Components/Footer";
 import Menu from "./Components/Navbar";
+import ScrollToTop from "react-scroll-to-top";
 
 function App() {
-  return (
-    <div style={{ background: "white" }}>
-      <Menu />
-      <HeadingSection />
-      <FeaturesSection />
-      <InstructionsSection />
-      <DiscoverSection />
-      <PhotoSection />
-      <ContactSection />
-      <Footer />
-    </div>
-  );
+	return (
+		<div style={{ background: "white" }}>
+			<ScrollToTop smooth color="#1960ff" />
+			<Menu />
+			<HeadingSection />
+			<FeaturesSection />
+			<InstructionsSection />
+			<DiscoverSection />
+			<PhotoSection />
+			<ContactSection />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,113 +1,97 @@
 import React from "react";
-import { withStyles } from "@material-ui/core/styles";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-scroll";
 import "./styles/NavbarStyles.css";
 
-import styles from "./styles/NavbarStyles";
+// import { useScrollPosition } from "../hooks";
 
-function Menu(props) {
-  const { classes } = props;
-  return (
-    <Navbar expand="lg" className={classes.navbar} bg="transparent">
-      <Navbar.Brand className={classes.navbarLogo} href="#">
-        <img
-          src="/assets/blue_appointment_icon.svg"
-          width="120"
-          height="100"
-          className="d-inline-block align-top"
-          alt="Logo"
-        />
-        <img
-          src="/assets/APPOINTMENT_Logo_Black.svg"
-          width="300"
-          height="100"
-          className="d-inline-block align-top"
-          alt="Logo"
-        />
-      </Navbar.Brand>
-      <Container>
-        <Nav className="mr-auto">
-          <Nav.Link
-            bsPrefix={`${classes.navbarLinksContainer} navlink`}
-            href="#"
-          >
-            <Link
-              className={classes.navbarLink}
-              to="HeadingSection"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-            >
-              Home
-            </Link>
-          </Nav.Link>
-          <Nav.Link
-            bsPrefix={`${classes.navbarLinksContainer} navlink`}
-            href="#"
-          >
-            <Link
-              className={classes.navbarLink}
-              to="FeatureSection"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-            >
-              Features
-            </Link>
-          </Nav.Link>
+function Menu() {
+	// const navbarLogo = "/assets/blue_appointment_icon.svg";
+	// const navbarLogoText = "/assets/APPOINTMENT_Logo_Black.svg";
+	return (
+		<Navbar expand="lg" className="navbar">
+			<Navbar.Brand className="navbarLogo" href="#">
+				<img
+					src="/assets/blue_appointment_icon.svg"
+					width="80"
+					height="60"
+					alt="Logo"
+				/>
+				<img
+					src="/assets/APPOINTMENT_Logo_Black.svg"
+					width="250"
+					height="100"
+					alt="Logo"
+				/>
+			</Navbar.Brand>
 
-          <Nav.Link
-            bsPrefix={`${classes.navbarLinksContainer} navlink`}
-            href="#"
-          >
-            <Link
-              className={classes.navbarLink}
-              to="InstructionsSection"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-            >
-              How it works?
-            </Link>
-          </Nav.Link>
-          <Nav.Link
-            bsPrefix={`${classes.navbarLinksContainer} navlink`}
-            href="#"
-          >
-            <Link
-              className={classes.navbarLink}
-              to="PhotoSection"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-            >
-              Screenshots
-            </Link>
-          </Nav.Link>
-          <Nav.Link
-            bsPrefix={`${classes.navbarLinksContainer} navlink`}
-            href="#"
-          >
-            <Link
-              className={classes.navbarLink}
-              to="ContactSection"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-            >
-              Contact Us
-            </Link>
-          </Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
+			<Container>
+				<Nav className="mr-auto">
+					<Nav.Link bsPrefix={`navbarLinksContainer navlink`} href="#">
+						<Link
+							className="navbarLink"
+							to="HeadingSection"
+							spy={true}
+							smooth={true}
+							offset={0}
+							duration={500}
+						>
+							Home
+						</Link>
+					</Nav.Link>
+					<Nav.Link bsPrefix={`navbarLinksContainer navlink`} href="#">
+						<Link
+							className="navbarLink"
+							to="FeatureSection"
+							spy={true}
+							smooth={true}
+							offset={0}
+							duration={500}
+						>
+							Features
+						</Link>
+					</Nav.Link>
+
+					<Nav.Link bsPrefix={`navbarLinksContainer navlink`} href="#">
+						<Link
+							className="navbarLink"
+							to="InstructionsSection"
+							spy={true}
+							smooth={true}
+							offset={0}
+							duration={500}
+						>
+							How it works?
+						</Link>
+					</Nav.Link>
+					<Nav.Link bsPrefix={`navbarLinksContainer navlink`} href="#">
+						<Link
+							className="navbarLink"
+							to="PhotoSection"
+							spy={true}
+							smooth={true}
+							offset={0}
+							duration={500}
+						>
+							Screenshots
+						</Link>
+					</Nav.Link>
+					<Nav.Link bsPrefix={`navbarLinksContainer navlink`} href="#">
+						<Link
+							className="navbarLink"
+							to="ContactSection"
+							spy={true}
+							smooth={true}
+							offset={0}
+							duration={500}
+						>
+							Contact Us
+						</Link>
+					</Nav.Link>
+				</Nav>
+			</Container>
+		</Navbar>
+	);
 }
 
-export default withStyles(styles)(Menu);
+export default Menu;
